addd3a1a9f27f50ed0c6dc525b42b5fd211029f9
bender <kudin.dima@gmail.com>
2026-05-20 10:13:42 +0000

    Update src/app/sovale/page.tsx

--- src/app/sovale/page.tsx
+++ src/app/sovale/page.tsx
@@ -36,12 +36,13 @@ export default function SovalePage() {
           animationType="slide-up"
           textboxLayout="split"
           gridVariant="three-columns-all-equal-width"
+          useInvertedBackground={false}
           title="Profesyonel Şövale Çözümleri"
           description="Atölye ve sergileriniz için dayanıklı, ergonomik ve estetik şövale modelleri."
           products={[
-            { id: "s1", name: "Masaüstü Şövale", price: "Teklif Al", imageSrc: "http://img.b2bpic.net/free-photo/artist-front-blank-canvas_23-2147781648.jpg?_wi=1", priceButtonProps: { href: "/iletisim" } },
-            { id: "s2", name: "Atölye Tipi Şövale", price: "Teklif Al", imageSrc: "http://img.b2bpic.net/free-photo/medium-shot-senior-painter-indoors_23-2150182578.jpg?_wi=1", priceButtonProps: { href: "/iletisim" } },
-            { id: "s3", name: "Sergi Tipi Şövale", price: "Teklif Al", imageSrc: "http://img.b2bpic.net/free-photo/red-deers-with-white-leaves-painted-wooden-board_1304-3707.jpg?_wi=1", priceButtonProps: { href: "/iletisim" } }
+            { id: "s1", name: "Masaüstü Şövale", price: "Teklif Al", imageSrc: "http://img.b2bpic.net/artist-front-blank-canvas_23-2147781648.jpg?_wi=1", priceButtonProps: { href: "/iletisim" } },
+            { id: "s2", name: "Atölye Tipi Şövale", price: "Teklif Al", imageSrc: "http://img.b2bpic.net/medium-shot-senior-painter-indoors_23-2150182578.jpg?_wi=1", priceButtonProps: { href: "/iletisim" } },
+            { id: "s3", name: "Sergi Tipi Şövale", price: "Teklif Al", imageSrc: "http://img.b2bpic.net/red-deers-with-white-leaves-painted-wooden-board_1304-3707.jpg?_wi=1", priceButtonProps: { href: "/iletisim" } }
           ]}
         />
         <ContactCenter
@@ -49,12 +50,15 @@ export default function SovalePage() {
           title="Özel Üretim Şövale mi Arıyorsunuz?"
           description="İhtiyacınız olan boyut ve tasarım için bize ulaşın."
           background={{ variant: "gradient-bars" }}
+          useInvertedBackground={false}
         />
         <FooterSimple
           columns={[
             { title: "Deniz Çerçeve", items: [{ label: "Anasayfa", href: "/" }, { label: "İletişim", href: "/iletisim" }] },
             { title: "Ürünler", items: [{ label: "Şövale", href: "/sovale" }] },
           ]}
+          bottomLeftText="© 2025 Deniz Çerçeve"
+          bottomRightText="Ankara"
         />
       </ReactLenis>
     </ThemeProvider>
